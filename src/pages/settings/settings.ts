fbb9d3db76131a12d44320103777b82731560e97
wkande <warren@wyomingsoftware.com>
2017-11-14 13:56:10 -0700

    Hints update

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { NavController, Events } from 'ionic-angular';
+import { NavController, Events, Platform } from 'ionic-angular';
 import { HintsComponent } from '../../components/hints';
+import { AppVersion } from '@ionic-native/app-version';
 
 
 @Component({
@@ -8,6 +9,12 @@ import { HintsComponent } from '../../components/hints';
   templateUrl: 'settings.html',
   providers:[HintsComponent]
 })
+
+
+/**
+ * App settings view.
+ * @type {View}
+ */
 export class SettingsPage {
 
 
@@ -15,34 +22,66 @@ export class SettingsPage {
   showHints:boolean;
   listRefresh:number;
   statsRefresh:number;
+  version:string = 'Cordova not available';
 
 
-  constructor(public navCtrl: NavController, public events:Events) {
+  /**
+   * Class constructor.
+   * @param  {NavController} navCtrl Ionic navigation controller
+   * @param  {Events}        events  Ionic events
+   * @param  {AppVersion}    appVersion  Ionic native plugin
+   * @param  {Platform}      platform  Ionic platform/cordova info
+   */
+  constructor(
+    public navCtrl: NavController,
+    public events:Events,
+    appVersion:AppVersion,
+    platform:Platform
+  ) {
     this.demoServers = (window.localStorage.getItem("noditor.demoServers") === 'true');
     this.showHints = (window.localStorage.getItem("noditor.showHints") === 'true');
     this.listRefresh = parseInt(window.localStorage.getItem("noditor.listRefresh")) || 7;
     this.statsRefresh = parseInt(window.localStorage.getItem("noditor.statsRefresh")) || 7;
+    
+    if(platform.is('cordova')){
+      var self = this;
+      appVersion.getVersionNumber().then(function (data) {
+          self.version = data;
+      });
+    }
   }
 
 
-  setListRefresh(){
+  /**
+   * Sets the value of the list refrersh flag and sends out an event.
+   */
+  setListRefresh():void{
     window.localStorage.setItem("noditor.listRefresh", this.listRefresh.toString());
     this.events.publish('listRefresh:changed', this.listRefresh);
   }
 
 
-  setStatsRefresh(){
+  /**
+   * Sets the value of the stats refrersh flag and sends out an event.
+   */
+  setStatsRefresh():void{
     window.localStorage.setItem("noditor.statsRefresh", this.statsRefresh.toString());
     this.events.publish('statsRefresh:changed', this.statsRefresh);
   }
 
 
-  setDemoServers(){
+  /**
+   * Sets the value of the show demos flag and sends out an event.
+   */
+  setDemoServers():void{
     window.localStorage.setItem("noditor.demoServers", this.demoServers.toString());
     this.events.publish('demoServers:changed', this.demoServers);
   }
 
 
+  /**
+   * Sets the value of the show hints flag and sends out an event.
+   */
   setShowHints(){
     window.localStorage.setItem("noditor.showHints", this.showHints.toString());
     this.events.publish('showHints:changed', this.showHints);
